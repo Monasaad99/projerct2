d005974ef986e124316ba00514385459019b0a91
Monasaad99 <121646100+Monasaad99@users.noreply.github.com>
2022-12-30 03:24:34 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace section1
+{
+    class program
+    {
+
+        static void Main(String[] args)
+        {
+            int number, sum = 0,n;
+            Console.WriteLine("Please enter the number");
+            int Number = int.Parse(Console.ReadLine());
+            n = Number;
+            int i = 1;
+            for (; i < Number; i++);
+            {
+                if (Number % i == 0)
+                {
+                    sum = sum + i ;
+                }
+            }
+            if (sum == n)
+            {
+                Console.WriteLine("entered number is a perfect number");
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("enterd number is not a perfect number");
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
+
+
+
+                
